6cec1eb8857de7c4d8b0b81075ccc0760c8b5471
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-06-17 20:42:56 -0300

    Verbos e Rotas

--- MimicAPI/Controllers/PalavrasController.cs
+++ MimicAPI/Controllers/PalavrasController.cs
@@ -1,11 +1,65 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using MimicAPI.Database;
+using MimicAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MimicAPI.Controllers
 {
-    public class PalavrasController
+    [Route("api/palavras")]
+    public class PalavrasController : ControllerBase
     {
+        private readonly MimicContext _banco;
+
+        public PalavrasController(MimicContext banco)
+        {
+            _banco = banco;
+        }
+
+        //APP
+        [Route("")]
+        [HttpGet]
+        public ActionResult ObterTodas()
+        {
+            return Ok(_banco.Palavras);
+        }
+
+        //Web
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult Obter(int id)
+        {
+            return Ok(_banco.Palavras.Find(id));
+        }
+
+        [Route("")]
+
+        [HttpPost]
+        public ActionResult Cadastrar(Palavra palavra)
+        {
+            _banco.Palavras.Add(palavra);
+            return Ok();
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public ActionResult Atualizar(int id, Palavra palavra)
+        {
+            palavra.Id = id;
+            _banco.Palavras.Update(palavra);
+            return Ok();
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public ActionResult Deletar(int id)
+        {
+            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+            return Ok();
+        }
+
+
     }
 }
