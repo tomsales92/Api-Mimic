3b465f471bd9dbf252784c2f2a94e4f9748f2c32
Everton Sales <everton.goncalves92@gmail.com>
2020-06-29 18:11:07 -0300

    Link Self

--- MimicAPI/Controllers/PalavrasController.cs
+++ MimicAPI/Controllers/PalavrasController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MimicAPI.Helpers;
 using MimicAPI.Models;
+using MimicAPI.Models.DTO;
 using MimicAPI.Repositories.Interfaces;
 using Newtonsoft.Json;
 using System;
@@ -15,10 +17,12 @@ namespace MimicAPI.Controllers
     public class PalavrasController : ControllerBase
     {
         private readonly IPalavraRepository _repository;
+        private readonly IMapper _mapper;
 
-        public PalavrasController(IPalavraRepository repository)
+        public PalavrasController(IPalavraRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         //APP
@@ -47,7 +51,16 @@ namespace MimicAPI.Controllers
             if (obj == null)
                 return NotFound();
 
-            return Ok(obj);
+            PalavraDTO palavraDTO = _mapper.Map<Palavra, PalavraDTO>(obj);
+
+            palavraDTO.Links = new List<LinkDTO>();
+      
+
+            palavraDTO.Links.Add(
+                new LinkDTO("self", $"https://localhost:44308/api/palavras/{palavraDTO.Id}", "GET")
+                );
+
+            return Ok(palavraDTO);
         }
 
         [Route("")]
